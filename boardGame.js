40b451c3ea3daea3316cf24d3afec5b439c5de24
Lady-Diane-Prado <92840695+Lady-Diane-Prado@users.noreply.github.com>
2022-02-14 08:51:01 +0800

    Update boardGame.js

--- boardGame.js
+++ boardGame.js
@@ -6,13 +6,31 @@ let askName = userInput.question("What is your name?");
 // make the first letter of name capitalize
 let playerName = askName.charAt(0).toUpperCase() + name.slice(1);
 // print name
-console.log(`Player Name: ${playerName});
+console.log(`Player Name: ${playerName}`);
 
 // create board
 /* Sample Board (3x3)
 [?, ?, ?]
 [?, ?, ?]
 [?, ?, ?]
-/*
+*/
 
-const 
+let playerBoard = [];
+let rowBoard = 3;
+let columnBoard = 3;
+
+// print rows
+for (let i = 0; i < rowBoard; i++) {
+  playerBoard.push([]);
+  // print columns
+  for (let j = 0; j < columnBoard; j++) {
+    playerBoard[i].push('?'); 
+  }
+}
+
+// print the board
+for (i = 0; i < rowColumn; i++) {
+  console.log(playerBoard[i]); 
+}
+
+
